59917d321f3ef5bad66d2b78ad45e4fd3310890e
xiaodu114514 <152014626+xiaodu114514@users.noreply.github.com>
2025-03-09 15:36:46 +0800

    Add files via upload

---
+++ untitled1.cpp
@@ -0,0 +1,16 @@
+#include "stdio.h"
+
+int main(){
+	printf("请输入一个数字");
+	float a,b;
+	scanf("%f",&a);
+	printf("请再输入一个数字");
+	scanf("%f",&b);
+	float c=a+b;
+	if(c==114514){
+		printf("哼哼啊啊啊啊啊啊啊啊啊啊");
+		//TODO
+	}else
+	printf("%f",c);
+	return 0;
+}
